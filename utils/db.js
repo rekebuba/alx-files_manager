72330e8c11a44de460764d06cf60f39cb95f155d
rekebuba <abukiabdullahi1@gmail.com>
2024-10-01 23:54:41 +0300

    0x04. Files manager

--- utils/db.js
+++ utils/db.js
@@ -1,39 +1,59 @@
 import mongodb from 'mongodb';
 // eslint-disable-next-line no-unused-vars
 import Collection from 'mongodb/lib/collection';
-import envLoader from './env_loader';
+import { existsSync, readFileSync } from 'fs';
+// import envLoader from './env_loader';
+
+/**
+ * Loads the appropriate environment variables for an event.
+ */
+const envLoader = () => {
+  const env = process.env.npm_lifecycle_event || 'dev';
+  const path = env.includes('test') || env.includes('cover') ? '.env.test' : '.env';
+
+  if (existsSync(path)) {
+    const data = readFileSync(path, 'utf-8').trim().split('\n');
+
+    for (const line of data) {
+      const delimPosition = line.indexOf('=');
+      const variable = line.substring(0, delimPosition);
+      const value = line.substring(delimPosition + 1);
+      process.env[variable] = value;
+    }
+  }
+};
 
 class DBClient {
-    constructor() {
-        envLoader();
-        const host = process.env.DB_HOST || 'localhost';
-        const port = process.env.DB_PORT || 27017;
-        const database = process.env.DB_DATABASE || 'files_manager';
-        const dbURL = `mongodb://${host}:${port}/${database}`;
+  constructor() {
+    envLoader();
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const dbURL = `mongodb://${host}:${port}/${database}`;
 
-        this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
-        this.client.connect();
-    }
+    this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
+    this.client.connect();
+  }
 
-    isAlive() {
-        return this.client.isConnected();
-    }
+  isAlive() {
+    return this.client.isConnected();
+  }
 
-    async nbUsers() {
-        return this.client.db().collection('users').countDocuments();
-    }
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
 
-    async nbFiles() {
-        return this.client.db().collection('files').countDocuments();
-    }
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
 
-    async usersCollection() {
-        return this.client.db().collection('users');
-    }
+  async usersCollection() {
+    return this.client.db().collection('users');
+  }
 
-    async filesCollection() {
-        return this.client.db().collection('files');
-    }
+  async filesCollection() {
+    return this.client.db().collection('files');
+  }
 }
 
 export const dbClient = new DBClient();
